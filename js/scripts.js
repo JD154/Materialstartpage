1a37a6159dcc953467ca8732f76c54a9ed410828
JD154 <danielrey3553@gmail.com>
2018-09-24 09:50:16 -0400

    replaced old getelementbyid with queryselector

--- js/scripts.js
+++ js/scripts.js
@@ -11,25 +11,25 @@ var set_id = "";        //create a variable to store name of id sections
 // Assign click event listener to each plus button in the page 
 dev_id.addEventListener('click', function dev_set() {
     openSideBar();      // Call function to open side bar 
-    set_id = "development";      // Set the appropriate name to id variable
+    set_id = "#development";      // Set the appropriate name to id variable
     console.log("ID = " + set_id);        //Verifies in console if the name is correct
 });
 
 reddit_id.addEventListener('click', function reddit_set() {
     openSideBar();
-    set_id = "reddit";
+    set_id = "#reddit";
     console.log("ID = " + set_id);
 });
 
 social_id.addEventListener('click', function social_set() {
     openSideBar();
-    set_id = "social";
+    set_id = "#social";
     console.log("ID = " + set_id);
 });
 
 others_id.addEventListener('click', function others_set() {
     openSideBar();
-    set_id = "others";
+    set_id = "#others";
     console.log("ID = " + set_id);
 });
 
@@ -47,17 +47,17 @@ url_id.addEventListener('click', function send_data(){
     console.log(domain_name);
     console.log(set_id);
 
-    var li = document.createElement("li");        //create a div element
-    li.className = "content-link";         //assign the appropiate class name to div
+    var li = document.createElement("li");        //create a li element
+    li.className = "content-link";         //assign the appropiate class name to li
 
     var anchor = document.createElement('a');       //create a anchor element
     anchor.setAttribute('href', url_name);      //use data from input field to set an href url
     anchor.innerHTML = domain_name;     //use data from input field to set text to this anchor
 
-    li.appendChild(anchor);        //append the anchor to the div created
+    li.appendChild(anchor);        //append the anchor to the li created
 
-    var element = document.getElementById(set_id);      //search for appropiate section with the id provided
-    element.appendChild(li);       //append created div to the appropiate section
+    var element = document.querySelector(set_id);      //search for appropiate section with the id provided
+    element.appendChild(li);       //append created li to the appropiate section
 
     /* Clear values */
     document.querySelector("#get-url").value = "";
