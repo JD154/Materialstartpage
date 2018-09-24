c014fa55ed0fef41016221eb1640deb2bb73f226
JD154 <danielrey3553@gmail.com>
2018-09-24 09:44:23 -0400

    replaced old getelementbyid by queryselector

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-/* Create variables searching for each section id*/
-const dev_id = document.getElementById("dev-id");
-const reddit_id = document.getElementById("reddit-id");
-const social_id = document.getElementById("social-id");
-const others_id = document.getElementById("others-id");
-const url_id = document.getElementById("send-data");
-const close_btn = document.getElementById("close-sidebar");
+// Create variables searching for each section id
+const dev_id = document.querySelector("#dev-id");
+const reddit_id = document.querySelector("#reddit-id")
+const social_id = document.querySelector("#social-id");
+const others_id = document.querySelector("#others-id");
+const url_id = document.querySelector("#send-data");
+const close_btn = document.querySelector("#close-sidebar");
 
 var set_id = "";        //create a variable to store name of id sections
 
-/* Assign click event listener to each plus button in the page */
+// Assign click event listener to each plus button in the page 
 dev_id.addEventListener('click', function dev_set() {
     openSideBar();      // Call function to open side bar 
     set_id = "development";      // Set the appropriate name to id variable
@@ -33,13 +33,16 @@ others_id.addEventListener('click', function others_set() {
     console.log("ID = " + set_id);
 });
 
-/* Function to get data from input fields when user press the pencil button on side bar */
-url_id.addEventListener('click', function send_data(){
-    /* Search content from input fields */
-    var url_name = document.getElementById("get-url").value;
-    var domain_name = document.getElementById("get-name").value;
+// Assign click event listener to X button on the side bar 
+close_btn.addEventListener('click', closeSideBar);
 
-    /* Verifies in console each value before final set */
+// Function to get data from input fields when user press the pencil button on side bar 
+url_id.addEventListener('click', function send_data(){
+    // Search content from input fields 
+    var url_name = document.querySelector("#get-url").value;
+    var domain_name = document.querySelector("#get-name").value;
+
+    // Verifies in console each value before final set 
     console.log(url_name);
     console.log(domain_name);
     console.log(set_id);
@@ -57,29 +60,28 @@ url_id.addEventListener('click', function send_data(){
     element.appendChild(li);       //append created div to the appropiate section
 
     /* Clear values */
-    document.getElementById("get-url").value = "";
-    document.getElementById("get-name").value = "";
+    document.querySelector("#get-url").value = "";
+    document.querySelector("#get-name").value = "";
     closeSideBar();
     set_id = "";
     console.log("values cleared!");
 });
 
-/* Assign click event listener to X button on the side bar */
-close_btn.addEventListener('click', closeSideBar);
 
-/* Function to set styles and make side bar visible */
+// Function to set styles and make side bar visible 
 function openSideBar(){
-    document.getElementById("mySidenav").style.transform = "translateX(0%)";
-    document.getElementById("sidebar-overlay").style.opacity = "1";
-    document.getElementById("sidebar-overlay").style.pointerEvents = "auto";
+    document.querySelector(".sidenav").style.transform = "translateX(0%)";
+    document.querySelector("#sidebar-overlay").style.opacity = "1";
+    document.querySelector("#sidebar-overlay").style.pointerEvents = "auto";
 
-    document.getElementById("sidebar-overlay").addEventListener('click', closeSideBar);
+    document.querySelector("#sidebar-overlay").addEventListener('click', closeSideBar);
 }
 
+// Function to set styles and make side bar invisible
 function closeSideBar() {
-    document.getElementById("mySidenav").style.transform = "translateX(-100%)";
-    document.getElementById("sidebar-overlay").style.opacity= "0";
-    document.getElementById("sidebar-overlay").style.pointerEvents = "none";
+    document.querySelector(".sidenav").style.transform = "translateX(-100%)";
+    document.querySelector("#sidebar-overlay").style.opacity= "0";
+    document.querySelector("#sidebar-overlay").style.pointerEvents = "none";
 }
 
 
