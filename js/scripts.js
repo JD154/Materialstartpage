670e294dfb93274ff9fbbee2ed7f323fe0007a4b
JD154 <danielrey3553@gmail.com>
2019-03-19 20:55:40 -0400

    added section header edit option in first stage

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,55 @@
 //create a variable to store name of id sections
 let setId = "";
+let selectedSection = "";
+let sectionsList = document.querySelectorAll(".box-header");
 
 // Add click event listener to each plus button in the page 
 document.querySelector("#dev-id").addEventListener('click', () => {
     openSideBar();      // Call function to open side bar 
-    setId = "#development";      // Set the appropriate name to id variable
+    setId = "#section1";      // Set the appropriate name to id variable
     console.log("ID = " + setId);        //Verifies in console if the name is correct
 });
 
 document.querySelector("#reddit-id").addEventListener('click', () => {
     openSideBar();
-    setId = "#reddit";
+    setId = "#section2";
     console.log("ID = " + setId);
 });
 
 document.querySelector("#social-id").addEventListener('click', () => {
     openSideBar();
-    setId = "#social";
+    setId = "#section3";
     console.log("ID = " + setId);
 });
 
 document.querySelector("#others-id").addEventListener('click', () => {
     openSideBar();
-    setId = "#others";
+    setId = "#section4";
     console.log("ID = " + setId);
+});
+
+document.querySelector("#modal-trigger").addEventListener('click', () => {
+    document.querySelector(".modal-overlay").style.display = "block";
+
+    document.querySelector("#dropdown-trigger").addEventListener('click', () =>{
+        document.querySelector("#page-sections").style.display = "block";
+    });
+    
+    document.querySelector("#close-modal").addEventListener('click', () => {
+        document.querySelector(".modal-overlay").style.display = "none";
+    });
 });
 
 // When load will check if fetched array from localStorage is not empty
 document.addEventListener('DOMContentLoaded', () => {
     getSite();
+    getSections();
 });
 
 // Function to get data from input fields when user press the pencil button on side bar 
 document.querySelector("#send-data").addEventListener('click', () => {
 
-    // Search content from input fields 
+    // Get content from input fields 
     let urlName = document.querySelector("#get-url").value;
     let domainName = document.querySelector("#get-name").value;
 
@@ -54,6 +69,24 @@ document.querySelector("#send-data").addEventListener('click', () => {
     // Clear values 
     clearValues();
 });
+
+// Function is called when I want to get data from LocalStorage objects and put in variables
+function getSite(){
+    let savedSites = JSON.parse(localStorage.getItem('savedSites'));        // Take object with sites and parse it to a string
+
+    if (savedSites != null){
+        for (const i in savedSites){
+
+            // Get values of the string in each iteration and saves in variables
+            let url = savedSites[i].url;
+            let domain = savedSites[i].domain;
+            let id = savedSites[i].id;
+
+            appendSite(url, domain, id);        // Call function to append data into the HTML
+            console.log(url + " " + domain + " " + id);     // Check in console each iteration of the loop
+        }
+    }
+}
 
 // Function allow me save data from input fields into an object in LocalStorage
 function saveSite(urlName, domainName, setId){
@@ -77,24 +110,6 @@ function saveSite(urlName, domainName, setId){
     }
 }
 
-// Function is called when I want to get data from LocalStorage objects and put in variables
-function getSite(){
-    let savedSites = JSON.parse(localStorage.getItem('savedSites'));        // Take object with sites and parse it to a string
-
-    if (savedSites != null){
-        for (const i in savedSites){
-
-            // Get values of the string in each iteration and saves in variables
-            let url = savedSites[i].url;
-            let domain = savedSites[i].domain;
-            let id = savedSites[i].id;
-
-            appendSite(url, domain, id);        // Call function to append data into the HTML
-            console.log(url + " " + domain + " " + id);     // Check in console each iteration of the loop
-        }
-    }
-}
-
 // Function to add fetched data from LocalStorage into HTML
 function appendSite(urlName, domainName, set_id){
     let li = document.createElement("li");        // Create a li element
@@ -115,7 +130,7 @@ function appendSite(urlName, domainName, set_id){
     element.appendChild(li);       // Append created li to the appropiate section
 }
 
-// Function to search for anchor content delete entire object from LocalStorage
+// Function to search for anchor content and delete entire object from LocalStorage
 function deleteSite(){
     let parent = this.parentElement;        // Search for the parent element of the icon
     let anchor = parent.firstChild.innerHTML;        // After search for the parent will search for the anchor as firstChild and get domain name
@@ -123,31 +138,23 @@ function deleteSite(){
     let savedSites = JSON.parse(localStorage.getItem('savedSites'));
 
     for (const i in savedSites) {
-
         if (savedSites[i].domain == anchor) {
             savedSites.splice(i, 1);
             console.log("li element deleted!");
         } 
 
-        localStorage.setItem('savedSites', JSON.stringify(savedSites));     // Save modified object in Local Storage
-        parent.remove();        //remove complete desired li element
+        localStorage.setItem('savedSites', JSON.stringify(savedSites));     // Save changes in Local Storage
+        parent.remove();        // Remove complete desired li element
     }
 }
 
 // Assign click event listener to X button on the side bar 
 document.querySelector("#close-sidebar").addEventListener('click', closeSideBar);
 
-// Function to set styles and make side bar visible 
-function openSideBar(){
-    document.querySelector(".sidenav").classList.add('sidenav-isvisible');
-    document.querySelector(".sidebar-overlay").classList.add('overlay-isvisible');
-    document.querySelector(".sidebar-overlay").addEventListener('click', closeSideBar);
-}
-
 // When an input field is empty and submit an error message will be displayed
 function errorMessage(){
     document.querySelector(".form-error").classList.add('form-error-isvisible');
-
+    
     setTimeout(() => {
         document.querySelector(".form-error").classList.remove('form-error-isvisible');
     }, 1500);
@@ -157,8 +164,15 @@ function errorMessage(){
 function clearValues(){
     document.querySelector("#get-url").value = "";
     document.querySelector("#get-name").value = "";
-
+    
     console.log("values cleared!");
+}
+
+// Function to set styles and make side bar visible 
+function openSideBar(){
+    document.querySelector(".sidenav").classList.add('sidenav-isvisible');
+    document.querySelector(".sidebar-overlay").classList.add('overlay-isvisible');
+    document.querySelector(".sidebar-overlay").addEventListener('click', closeSideBar);
 }
 
 // Function to set styles and make side bar invisible
@@ -166,4 +180,72 @@ function closeSideBar() {
     document.querySelector(".sidenav").classList.remove('sidenav-isvisible');
     document.querySelector(".sidebar-overlay").classList.remove('overlay-isvisible');
     clearValues();
+}
+
+function getSections(){
+    reprintSections();
+    let list = [].slice.call(sectionsList);
+    let innerSections = list.map(e => e.innerHTML);
+
+    saveSections(innerSections);
+
+    document.querySelectorAll("#page-sections > li").forEach(e => e.remove());
+    for (const i in innerSections) {
+        let liItem = document.createElement("li");
+
+        liItem.addEventListener('click', () =>{
+            document.querySelector("#page-sections").style.display = "none";
+
+            selectedSection = liItem.innerHTML;
+            document.querySelector("#dropdown-trigger").innerHTML = selectedSection;
+            console.log("Section to edit: " + selectedSection);
+        });
+
+        liItem.innerHTML = innerSections[i];
+        document.querySelector("#page-sections").appendChild(liItem);
+    }
+}
+
+function saveSections(sectionsArray){
+
+    let saveSection;
+    let savedSections = [];
+
+    if (localStorage.getItem('savedSections') === null){
+        for (const i in sectionsArray) {
+            saveSection = {
+                sectionName: sectionsArray[i]
+            }
+            savedSections.push(saveSection);
+            localStorage.setItem('savedSections', JSON.stringify(savedSections));  
+        }
+    }
+}
+
+document.querySelector("#section-change").addEventListener('click', () => {
+    let nameToChange = document.querySelector(".modal-input").value;
+    
+    if (nameToChange && selectedSection){
+        let savedSections = JSON.parse(localStorage.getItem('savedSections'));
+
+        for (const i in savedSections) {
+            if (savedSections[i].sectionName == selectedSection) {
+                savedSections[i].sectionName = nameToChange;
+            } 
+    
+            localStorage.setItem('savedSections', JSON.stringify(savedSections));     // Save changes in Local Storage
+        }
+
+    }
+    getSections();
+    document.querySelector(".modal-overlay").style.display = "none";
+    document.querySelector(".modal-input").value = "";
+    document.querySelector("#dropdown-trigger").innerHTML = "Select section";
+});
+
+function reprintSections(){
+    let savedSections = JSON.parse(localStorage.getItem('savedSections'));
+    for (const i in savedSections) {
+        sectionsList[i].innerHTML = savedSections[i].sectionName;
+    }
 }
